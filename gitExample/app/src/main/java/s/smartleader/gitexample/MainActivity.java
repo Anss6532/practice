5d9d5adcf7ca237ed5913e1551407f0d1603515e
zojae031 <edgar3@naver.com>
2018-12-22 19:16:06 +0900

    android exam

---
+++ gitExample/app/src/main/java/s/smartleader/gitexample/MainActivity.java
@@ -0,0 +1,23 @@
+package s.smartleader.gitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.a);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
